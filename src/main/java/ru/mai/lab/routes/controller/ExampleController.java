3091aa41ed58ec4ee4ca3dd6e20e412e8980c895
Wellusha <36533768+Wellusha@users.noreply.github.com>
2020-12-02 13:15:49 +0300

    Feature/add simple routes calculator (#6)
    * added stub for routes calculations
    
    * removed server port
    
    Co-authored-by: Vlad Shtender <Shtender.v.a@sberbank.ru>

--- src/main/java/ru/mai/lab/routes/controller/ExampleController.java
+++ src/main/java/ru/mai/lab/routes/controller/ExampleController.java
@@ -1,12 +1,22 @@
 package ru.mai.lab.routes.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.mai.lab.routes.entity.Route;
+import ru.mai.lab.routes.service.RouteService;
+
+import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class ExampleController {
+    private final RouteService routeService;
 
     @GetMapping("/example")
     public String example(@RequestParam String exampleString, Model model) {
@@ -14,4 +24,12 @@ public class ExampleController {
         return "example";
     }
 
+    @GetMapping("/routes/get")
+    public String getRoutes(@RequestParam(required = false) Integer day, Model model) {
+        List<Route> routes = routeService.getRoutes(day);
+        // возможно, нужно будет использовать как-то по-другому
+        model.addAttribute("routes", routes);
+        // здесь вместо null должно быть имя страницы на thymeleaf
+        return null;
+    }
 }
